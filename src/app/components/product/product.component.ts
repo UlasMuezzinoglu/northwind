bb275023fba9ecdd20c396e3b3736dfc38755c59
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-08-04 21:15:32 +0300

    Added Product Services

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-product',
@@ -10,10 +12,26 @@ export class ProductComponent implements OnInit {
 
 
   products: Product[] = []
+  apiUrl = "https://localhost:44341/api/Products/getall"
 
-  constructor() { }
+// productResponseModel:ProductResponseModel = {
+//   data : this.products,
+//   message:"",
+//   success:true
+// }
+
+
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    //console.log("init çalıştı");
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
+      this.products = response.data
+    });
   }
 
 }
