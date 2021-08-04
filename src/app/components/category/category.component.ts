56a0ebd7eb4821876236389329a6cbd26fcb5bef
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-08-04 20:32:53 +0300

    first Commit

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  categories:any=[{categoryId :1},{categoryId :3},{categoryId:5}]
+
+}
